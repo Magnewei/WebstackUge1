c13ef578e431a65f2378577a3a382c66351e2674
Magnewei <>
2024-03-04 22:53:56 +0100

    First commit

---
+++ Mandag/src/script.js
@@ -0,0 +1,14 @@
+
+
+
+// Insert number button onto the calculator.
+function GenerateNumberButton(number) {
+
+}
+
+// Calculate the input of the textfield. 
+// Using varagg to parse all elements.
+function Calculate(...arr) {
+
+}
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+### Webstack Frontend Uge
