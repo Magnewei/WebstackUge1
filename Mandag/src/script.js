6471771179c8a3d19f6e6136f5af9fa65b1e7632
Magnewei <Magnewei@Icloud.com>
2024-03-05 21:32:52 +0100

    More fixes

--- Mandag/src/script.js
+++ Mandag/src/script.js
@@ -1,14 +1,6 @@
-
-
-
 // Insert number button onto the calculator.
-function GenerateNumberButton(number) {
+function GenerateNumberButton(number) {}
 
-}
-
-// Calculate the input of the textfield. 
+// Calculate the input of the textfield.
 // Using varagg to parse all elements.
-function Calculate(...arr) {
-
-}
-
+function Calculate(...arr) {}
